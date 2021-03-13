69b701d217ac511d3d2f0b6cd2081a82f20db00f
sitek94 <msitkowski94@gmail.com>
2021-03-13 13:42:48 +0100

    Create simple Subscriber example

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { createStore, createSubscriber } from 'react-sweet-state';
+
+const Store = createStore({
+  initialState: {
+    count: 0,
+  },
+  actions: {
+    increment: () => ({ setState, getState }) => {
+      setState({ count: getState().count + 1 });
+    },
+  },
+  name: 'main',
+});
+
+const CounterSubscriber = createSubscriber(Store);
+
 function App() {
-  return <div>App</div>;
+  return (
+    <CounterSubscriber>
+      {({ count }, { increment }) => (
+        <div>
+          <h1>Count: {count}</h1>
+          <button onClick={increment}>increment</button>
+        </div>
+      )}
+    </CounterSubscriber>
+  );
 }
 
 export default App;
