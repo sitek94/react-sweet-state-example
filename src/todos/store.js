8d8044659b67540f3223f93f374178fa74750883
sitek94 <msitkowski94@gmail.com>
2021-03-13 16:32:11 +0100

    create and use generic actions

--- src/todos/store.js
+++ src/todos/store.js
@@ -1,35 +1,49 @@
 import { createStore, createContainer, createHook } from 'react-sweet-state';
 
+// Initial state
 const initialState = {
   data: [],
   status: 'idle',
   error: null,
 };
 
+// Generic actions
+const setStatus = status => ({ setState }) => {
+  setState({ status });
+};
+
+const setData = data => ({ setState }) => {
+  setState({
+    status: 'success',
+    data,
+  });
+};
+
+const setError = error => ({ setState }) => {
+  setState({
+    status: 'error',
+    error,
+  });
+};
+
+// Actions
 const actions = {
-  fetchTodos: () => async ({ setState, getState }, { api }) => {
+  fetchTodos: () => async ({ getState, dispatch }, { api }) => {
     if (getState().status === 'pending') return;
 
-    setState({
-      status: 'pending',
-    });
+    dispatch(setStatus('pending'));
 
     try {
       const todos = await api.getTodos();
 
-      setState({
-        status: 'success',
-        data: todos,
-      });
-    } catch (e) {
-      setState({
-        status: 'error',
-        error: e,
-      });
+      dispatch(setData(todos));
+    } catch (error) {
+      dispatch(setError(error));
     }
   },
 };
 
+// Store
 const TodosStore = createStore({
   initialState,
   actions,
