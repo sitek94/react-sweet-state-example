121641ffdb852354af05ce629843ab25a8cf4790
sitek94 <msitkowski94@gmail.com>
2021-03-13 17:12:19 +0100

    enable dev tools

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { defaults } from 'react-sweet-state';
 import App from './App';
+
+defaults.devtools = true;
 
 ReactDOM.render(
   <React.StrictMode>
